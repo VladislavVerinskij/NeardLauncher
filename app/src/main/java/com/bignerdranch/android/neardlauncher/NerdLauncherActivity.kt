c445b7b1f027b65cd48d79dae01a06df96ac0853
Vladislav <vladislav.verinskij@gmail.com>
2022-11-01 09:05:39 +0300

    fixed a bug

--- app/src/main/java/com/bignerdranch/android/neardlauncher/NerdLauncherActivity.kt
+++ app/src/main/java/com/bignerdranch/android/neardlauncher/NerdLauncherActivity.kt
@@ -1,10 +1,13 @@
 package com.bignerdranch.android.neardlauncher
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
+
+private const val TAG = "NerdLauncherActivity"
 class NerdLauncherActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
@@ -16,4 +19,16 @@ class NerdLauncherActivity : AppCompatActivity() {
         recyclerView = findViewById(R.id.app_recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(this)
     }
+
+    private fun setupAdapter() {
+        val startupIntent = Intent(Intent.ACTION_MAIN).apply {
+            addCategory(Intent.CATEGORY_LAUNCHER)
+        }
+
+        val activities = packageManager.queryIntentActivities(startupIntent,0)
+
+
+    }
+
+
 }
